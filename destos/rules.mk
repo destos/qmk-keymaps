6c7404db299568f67c752735740a4dd4319914d1
Patrick Forringer <patrick@forringer.com>
2022-08-02 15:49:10 -0500

    Update rules.mk

--- destos/rules.mk
+++ destos/rules.mk
@@ -1,7 +1,7 @@
 # Enable the Elite-C board
 BOOTLOADER = atmel-dfu
 MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = no # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE = yes # Enable WS2812 RGB underlight.
 OLED_ENABLE     = no
 OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
